64a364dfb6ffedbb1eee580cf6997544ca852ab7
aopoku255 <aopoku255@gmail.com>
2023-10-25 13:42:35 +0000

    Done

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    // baseURL: "https://geolocation.cyclic.app/",
+    baseURL: "https://sassty.cyclic.app/",
     // baseURL: "http://localhost:3001",
     headers: {
         "Content-Type": "application/json",
